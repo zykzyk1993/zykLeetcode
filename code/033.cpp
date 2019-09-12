e9c0ed3bf08a3046d639939c7d37c6879d94c983
zykzyk1993 <584853578@qq.com>
2019-09-12 22:40:44 +0800

    Update 033.cpp

--- code/033.cpp
+++ code/033.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n=nums.size(),minIndex,low=0,high=n-1,mid;
+        //判断特殊情况
         if(n==0)return -1;
+        //先二分找到minIndex然后再二分查找
         if(nums[0]<nums[n-1]){
+            //特殊情况：递增数组
             while(low<=high){
                 mid=low+(high-low)/2;
                 if(nums[mid]==target){
@@ -15,7 +18,9 @@ public:
                 }
             }
         }else{
+            //去掉尾部与nums[0]相等的数值
             while(high>0&&nums[high]==nums[0])high--;
+            //找minIndex过程注意二分条件
             while(low<high){
                 mid=low+(high-low)/2;
                 if(nums[mid]>nums[high]){
@@ -24,7 +29,9 @@ public:
                     high=mid;
                 }
             }
+            //记录minIndex
             minIndex=high;
+            //根据target大小确定二分查找的空间
             if(target>=nums[0]){
                 low=0;
                 high=minIndex;
