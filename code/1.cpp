9be04e2bd966851ae3ead5738080e6f4e4153374
zykzyk1993 <584853578@qq.com>
2019-09-12 10:27:43 +0800

    Create 1.cpp

---
+++ code/1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(m.count(target-nums[i])){
+                ans.push_back(m[target-nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            m[nums[i]]=i;
+        }
+        return ans;
+    }
+};
