5713a4ee1807dd0cc34aebc6856c388ef53cdd3a
zykzyk1993 <584853578@qq.com>
2019-09-14 14:12:19 +0800

    Create 064.cpp

---
+++ code/064.cpp
@@ -0,0 +1,38 @@
+
+//二维动态规划
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n,0));
+        dp[0][0]=grid[0][0];
+        for(int i=1;i<m;i++)dp[i][0]=dp[i-1][0]+grid[i][0];
+        for(int j=1;j<n;j++)dp[0][j]=dp[0][j-1]+grid[0][j];
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
+
+//一维动态规划
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<int> dp(n,0);
+        dp[0]=grid[0][0];
+        for(int j=1;j<n;j++)dp[j]=dp[j-1]+grid[0][j];
+        for(int i=1;i<m;i++){
+            dp[0]=dp[0]+grid[i][0];
+            for(int j=1;j<n;j++){
+                dp[j]=min(dp[j],dp[j-1])+grid[i][j];
+            }
+        }
+        return dp[n-1];
+    }
+};
