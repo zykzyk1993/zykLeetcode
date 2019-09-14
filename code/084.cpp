5d6bb11a6e823ab3e98290319d0e7ff308f01343
zykzyk1993 <584853578@qq.com>
2019-09-14 15:40:28 +0800

    Update 084.cpp

--- code/084.cpp
+++ code/084.cpp
@@ -1,7 +1,10 @@
+//单调栈
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         stack<int> s;
+        //为了方便
+        heights.push_back(-1);
         int maxArea=0,n=heights.size();
         for(int i=0;i<n;i++){
             while(!s.empty()&&heights[s.top()]>heights[i]){
@@ -12,12 +15,34 @@ public:
             }
             s.push(i);
         }
-        while(!s.empty()){
-            int h=heights[s.top()];
-            s.pop();
-            int left=s.empty()?0:s.top()+1;
-            maxArea=max(maxArea,(n-left)*h);
-        }
         return maxArea;
+    }
+};
+
+//双单调栈
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        if(heights.size()==0)return 0;
+        int ans=0;
+        stack<int> left,right;
+        vector<int> leftVol(heights.size(),0),rightVol(heights.size(),0);
+        for(int i=0;i<heights.size();i++){
+            int leftBound=-1;
+            while(!left.empty()&&heights[left.top()]>=heights[i])left.pop();
+            if(!left.empty())leftBound=left.top();
+            leftVol[i]=heights[i]*(i-leftBound-1);
+            left.push(i);
+        }
+        for(int i=heights.size()-1;i>=0;i--){
+            int rightBound=heights.size();
+            while(!right.empty()&&heights[right.top()]>=heights[i])right.pop();
+            if(!right.empty())rightBound=right.top();
+            rightVol[i]=heights[i]*(rightBound-i-1);
+            right.push(i);
+        }
+        for(int i=0;i<heights.size();i++)
+            ans=max(ans,leftVol[i]+rightVol[i]+heights[i]);
+        return ans;
     }
 };
