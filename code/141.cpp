35068d116e3f318b20daf1c41e4bffec7e652738
zykzyk1993 <584853578@qq.com>
2019-10-04 21:43:52 +0800

    Create 141.cpp

---
+++ code/141.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head)return false;
+        ListNode* fast=head,*slow=head;
+        while(fast&&fast->next){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)return true;
+        }
+        return false;
+    }
+};
