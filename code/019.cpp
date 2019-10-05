805c40644a59fa4941b7ed1fbc8962c2506b0dd1
zykzyk1993 <584853578@qq.com>
2019-10-05 12:21:22 +0800

    Update 019.cpp

--- code/019.cpp
+++ code/019.cpp
@@ -1,3 +1,10 @@
+ /*
+ * 19. 删除链表的倒数第N个节点
+ * 题意：删除链表的倒数第N个节点
+ * 难度：简单
+ * 分类：链表，快慢指针
+ * 算法：快慢指针，快指针达到链表尾部时，慢指针所在位置即为操作的节点，注意声明dummy更方便
+ */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
