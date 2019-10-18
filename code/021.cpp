e2c46a01312575354d4d1e4b63c8a5196aba02dc
zykzyk1993 <584853578@qq.com>
2019-10-18 15:40:20 +0800

    Update 021.cpp

--- code/021.cpp
+++ code/021.cpp
@@ -1,3 +1,10 @@
+ /*
+ * 21. 合并两个有序链表
+ * 题意：合并两个有序链表为一个单链表
+ * 难度：简单
+ * 分类：链表
+ * 算法：两个指针向后移动，拼接，记得最后判空拼接
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
