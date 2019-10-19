951cd7789f6f43af37f55e9e7be391dfbcb4c9eb
zykzyk1993 <584853578@qq.com>
2019-10-19 11:04:17 +0800

    Update 023.cpp

--- code/023.cpp
+++ code/023.cpp
@@ -1,3 +1,11 @@
+ /*
+ * 23. 合并K个排序链表
+ * 题意：给定括号对数n, 生成所有可能的并且有效的括号组合
+ * 难度：困难
+ * 分类：链表 优先队列
+ * 算法：将val和链表序号组成pair输入优先队列，排队输入
+ */
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
