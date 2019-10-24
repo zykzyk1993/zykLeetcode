15e87911362dbe3624d76dd2bc80c317637e8e87
zykzyk1993 <584853578@qq.com>
2019-10-24 22:00:06 +0800

    Update 039.cpp

--- code/039.cpp
+++ code/039.cpp
@@ -1,3 +1,10 @@
+/*
+ * 39. 组合总和
+ * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合
+ * 难度：中等
+ * 分类：dfs 剪枝
+ * 算法：传统dfs 剪枝 回溯
+ */
 class Solution {
 public:
     void dfs(vector<int> &candidates, vector<int> curSet, int target, int index, vector<vector<int>> &res){
