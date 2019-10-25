2c02dbb1b743a233c37eacc1fb6e96f7ee9dd072
zykzyk1993 <584853578@qq.com>
2019-10-25 11:49:13 +0800

    Update 042.cpp

--- code/042.cpp
+++ code/042.cpp
@@ -1,3 +1,10 @@
+/*
+ * 42. 接雨水
+ * 计算柱形图所能装下的雨水数量
+ * 难度：困难
+ * 分类：数组 栈 双指针
+ * 算法：三种方法:1.DP先求出来每个位置的maxleft,maxright，再遍历一遍;2.两个指针，类似lc11题的思路;3.用栈数据结构;
+ */
 class Solution {
 public:
     int trap(vector<int>& height) {
@@ -29,3 +36,42 @@ public:
         return ans;
     }
 };
+
+//栈 解法
+int trap(vector<int>& height)
+{
+    int ans = 0, current = 0;
+    stack<int> st;
+    while (current < height.size()) {
+        while (!st.empty() && height[current] > height[st.top()]) {
+            int top = st.top();
+            st.pop();
+            if (st.empty())
+                break;
+            int distance = current - st.top() - 1;
+            int bounded_height = min(height[current], height[st.top()]) - height[top];
+            ans += distance * bounded_height;
+        }
+        st.push(current++);
+    }
+    return ans;
+}
+//双指针 解法
+int trap(vector<int>& height)
+{
+    int left = 0, right = height.size() - 1;
+    int ans = 0;
+    int left_max = 0, right_max = 0;
+    while (left < right) {
+        if (height[left] < height[right]) {
+            height[left] >= left_max ? (left_max = height[left]) : ans += (left_max - height[left]);
+            ++left;
+        }
+        else {
+            height[right] >= right_max ? (right_max = height[right]) : ans += (right_max - height[right]);
+            --right;
+        }
+    }
+    return ans;
+}
+
