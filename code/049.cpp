00d5a971c4c08977c1b39390b22e640191a510fb
zykzyk1993 <584853578@qq.com>
2019-10-26 11:26:41 +0800

    Update 049.cpp

--- code/049.cpp
+++ code/049.cpp
@@ -1,3 +1,10 @@
+/*
+ * 49. 字母异位词分组
+ * 题意：相同字母组合的字符串分到一个组里
+ * 难度：中等
+ * 分类：哈希表 字符串
+ * Tips：思路很直接，用map即可
+ */
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
