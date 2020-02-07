f00c81d21d37c65e6c9b0f211ec56371c634e238
zykzyk1993 <584853578@qq.com>
2020-02-07 17:49:53 +0800

    Update 169.cpp

--- code/169.cpp
+++ code/169.cpp
@@ -1,3 +1,10 @@
+ /*
+ * 169. 多数元素
+ * 查找数组中的多数元素
+ * 难度：简单
+ * 分类：数组
+ * 算法：记录当前数字出现的cnt，若不是则减1，否则加1，清零后取下一个数字，最后会留下多数元素
+ */
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
