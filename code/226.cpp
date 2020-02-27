d6f63828a30a18c52cd2e0585aa6d0f032a91951
zykzyk1993 <584853578@qq.com>
2020-02-27 13:19:04 +0800

    Create 226.cpp

---
+++ code/226.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root)return NULL;
+        TreeNode* temp = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(temp);
+        return root;
+    }
+};
