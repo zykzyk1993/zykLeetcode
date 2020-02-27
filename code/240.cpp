46169945138cd74b3e61ae3eda2afd2af2a75cdd
zykzyk1993 <584853578@qq.com>
2020-02-27 13:24:12 +0800

    Create 240.cpp

---
+++ code/240.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        if(!m)return false;
+        int n=matrix[0].size();
+        if(!n)return false;
+        int i=0,j=n-1;
+        while(i<m&&j>=0){
+            if(matrix[i][j]==target)return true;
+            if(matrix[i][j]<target)i++;
+            else j--;
+        }
+        return false;
+    }
+};
