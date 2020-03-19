1eaadbd281f2c7ddb0c9f8a691e3b7c8d32caab3
zykzyk1993 <584853578@qq.com>
2020-03-19 16:29:57 +0800

    Update 215.cpp

--- code/215.cpp
+++ code/215.cpp
@@ -1,3 +1,10 @@
+/*
+ * 215. 数组中的第K个最大元素
+ * 在未排序的数组中找到第 k 个最大的元素
+ * 难度：中等
+ * 分类：数组 快排
+ * 算法：使用快排的partition，并采用左右指针的简洁写法
+ */
 class Solution {
 public:
     int partition(vector<int> &nums, int start, int end){
