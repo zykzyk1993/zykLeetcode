16ce26293e76c9f5570784db19c861bf963ecc3e
zykzyk1993 <584853578@qq.com>
2020-03-19 15:55:12 +0800

    Create 448.cpp

---
+++ code/448.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> res;
+        if(nums.empty()) return nums;
+        for(int i=0;i<nums.size();i++)
+        {
+            int index=(nums[i]-1)%nums.size();
+            nums[index]+=nums.size();
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<=nums.size())
+                res.push_back(i+1);
+        }
+        return res;
+    }
+};
