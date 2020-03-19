8f4f7eb1431dce57caf653982d11d86a397dedbd
zykzyk1993 <584853578@qq.com>
2020-03-19 16:25:35 +0800

    Update 207.cpp

--- code/207.cpp
+++ code/207.cpp
@@ -1,3 +1,10 @@
+ /*
+ * 207. 课程表
+ * 给定课程总量以及它们的先决条件，判断是否可能完成所有课程的学习
+ * 难度：中等
+ * 分类：图 dfs 拓扑排序
+ * 算法：经典拓扑排序，使用队列
+ */
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
