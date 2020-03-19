de15ab33b87a36345f675f599db054d17e34b406
zykzyk1993 <584853578@qq.com>
2020-03-19 16:36:18 +0800

    Update 236.cpp

--- code/236.cpp
+++ code/236.cpp
@@ -1,3 +1,10 @@
+/*
+ * 236. 二叉树的最近公共祖先
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先
+ * 难度：中等
+ * 分类：树
+ * 算法：采用注释的递归方法更加简洁，向上标记传递可能的结果
+ */
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
