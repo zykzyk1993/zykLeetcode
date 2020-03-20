9a8625d6428b02733da6aa3a10b90c2dd11ee92a
zykzyk1993 <584853578@qq.com>
2020-03-20 11:26:56 +0800

    Update 338.cpp

--- code/338.cpp
+++ code/338.cpp
@@ -1,3 +1,10 @@
+/*
+ * 338. 比特位计数
+ * 对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回
+ * 难度：中等
+ * 分类：位运算 dp
+ * 算法：注意每个数可以由i/2的数递推
+ */
 class Solution {
 public:
     vector<int> countBits(int num) {
