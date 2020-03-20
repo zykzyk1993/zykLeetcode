cc93fc4ca19180bd1f4e42160ec665c0796ad065
zykzyk1993 <584853578@qq.com>
2020-03-20 11:28:11 +0800

    Update 347.cpp

--- code/347.cpp
+++ code/347.cpp
@@ -1,3 +1,10 @@
+/*
+ * 347. 前 K 个高频元素
+ * 给定一个非空的整数数组，返回其中出现频率前 k 高的元素
+ * 难度：中等
+ * 分类：堆 哈希
+ * 算法：哈希表计数，然后用堆找出topK
+ */
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
