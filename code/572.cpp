bde79834d4789fb1b373e76019cb36c2224c822c
zykzyk1993 <584853578@qq.com>
2020-05-10 10:46:38 +0800

    Update 572.cpp

--- code/572.cpp
+++ code/572.cpp
@@ -1,3 +1,10 @@
+/*
+ * 572. 另一个树的子树
+ * 一棵树t是否是另一棵树s的子树，子树表示结构完全一致
+ * 难度：简单
+ * 分类：二叉树
+ * 算法：构造辅助函数判断是否为相同的树，然后递归判断子树
+ */
 class Solution {
 public:
     bool isSameTree(TreeNode* s, TreeNode* t){
