8ee721f36b0f131b2a19d2b7b3f5d8ec90db70ce
zykzyk1993 <584853578@qq.com>
2020-05-10 10:44:57 +0800

    Update 560.cpp

--- code/560.cpp
+++ code/560.cpp
@@ -1,3 +1,10 @@
+/*
+ * 560. 和为K的子数组
+ * 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数
+ * 难度：中等
+ * 分类：前缀和
+ * 算法：典型前缀和，保存前缀和为sum的数量，并每次累加前缀和为sum-k的数量
+ */
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
