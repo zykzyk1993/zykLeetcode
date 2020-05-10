95f00279d76349a636bbf95933b7fb8a61a0e5fa
zykzyk1993 <584853578@qq.com>
2020-05-10 09:50:54 +0800

    Update 448.cpp

--- code/448.cpp
+++ code/448.cpp
@@ -1,3 +1,11 @@
+  
+/*
+ * 448. 找到所有数组中消失的数字
+ * 给定数字范围 [1, n] 的数组，找到所有在 [1, n] 范围之间没有出现在数组中的数字，不使用额外空间
+ * 难度：简单
+ * 分类：数组
+ * 算法：每次出现一个数字，对应位加上n，最后检查，即可不使用额外空间
+ */
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
