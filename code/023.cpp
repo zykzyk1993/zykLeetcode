48ff109c3c8715fa9191e37a9176885f9a93a65c
zykzyk1993 <584853578@qq.com>
2021-09-29 13:32:13 +0800

    Update 023.cpp

--- code/023.cpp
+++ code/023.cpp
@@ -4,6 +4,7 @@
  * 难度：困难
  * 分类：链表 优先队列
  * 算法：将val和链表序号组成pair输入优先队列，排队输入
+ * 也可以每次pop的时候将后继节点push
  */
 
 /**
@@ -45,6 +46,47 @@ public:
                 lists[p.second]=lists[p.second]->next;
             }
             pre=pre->next;
+        }
+        return dummy->next;
+    }
+};
+
+//2021
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Compare_Node
+{
+public:
+    bool operator () (ListNode * l1, ListNode * l2)
+    {
+        return l1->val > l2->val;
+    }
+};
+priority_queue<ListNode*, vector<ListNode*>, Compare_Node> pq;
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode * dummy = new ListNode(-1);
+        ListNode * head = dummy;
+        for(auto &p:lists){
+            if(p){
+                pq.push(p);
+            }
+        }
+        while(!pq.empty()){
+            ListNode *temp = pq.top();
+            pq.pop();
+            head->next = temp;
+            if(temp->next)pq.push(temp->next);
+            head = head->next;
         }
         return dummy->next;
     }
