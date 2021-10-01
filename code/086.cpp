0ba1aa7364ac3560efb722ce06a50feacf78c0f1
zykzyk1993 <584853578@qq.com>
2021-10-01 11:38:33 +0800

    Create 086.cpp

---
+++ code/086.cpp
@@ -0,0 +1,39 @@
+/*
+ * 86. 分隔链表
+ * 题意：使大于或等于x的节点都在小于x的节点之后
+ * 难度：中等
+ * 分类：链表 模拟
+ * 算法：两个指针，模拟，合成
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* small = new ListNode(0);
+        ListNode* smallHead = small;
+        ListNode* large = new ListNode(0);
+        ListNode* largeHead = large;
+        while (head != nullptr) {
+            if (head->val < x) {
+                small->next = head;
+                small = small->next;
+            } else {
+                large->next = head;
+                large = large->next;
+            }
+            head = head->next;
+        }
+        large->next = nullptr;
+        small->next = largeHead->next;
+        return smallHead->next;
+    }
+};
