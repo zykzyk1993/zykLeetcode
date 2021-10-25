a3d78f99b074df8bf635264f693ebb616a64c1f2
zykzyk1993 <584853578@qq.com>
2021-10-25 20:56:39 +0800

    Update 448.cpp

--- code/448.cpp
+++ code/448.cpp
@@ -24,3 +24,23 @@ public:
         return res;
     }
 };
+
+//用负数标记也可以
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        for(auto &num:nums){
+            int pos = abs(num) - 1;
+            if(nums[pos] > 0){
+                nums[pos] = -nums[pos];
+            }
+        }
+        for(int i = 0; i < nums.size(); i ++) {
+            if(nums[i] > 0){
+                ans.push_back(i + 1);
+            }
+        }
+        return ans;
+    }
+};
