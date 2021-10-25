ce8f67277617329102a8a14f477d1f59e6bfd3a6
zykzyk1993 <584853578@qq.com>
2021-10-25 21:26:45 +0800

    Update 149.cpp

--- code/149.cpp
+++ code/149.cpp
@@ -4,7 +4,7 @@
  * 难度：困难
  * 分类：数学 哈希表
  * 算法：哈希表记录，以当前结点为第一个结点，记录后续结点的斜率
- * 注意，相同的点可以加入每一条直线，垂直的点没有斜率，要单独记录
+ * 注意，垂直的点没有斜率，要单独记录
  */
 class Solution {
 public:
@@ -15,10 +15,6 @@ public:
             unordered_map<double, int> m;
             int start_points = 1, vertical_points = 0, other_points = 0;
             for(int j = i + 1; j < n; j ++){
-                if(points[j] == points[i]){
-                    start_points++;
-                    continue;
-                }
                 if(points[j][0] == points[i][0]){
                     vertical_points ++;
                     continue;
@@ -27,7 +23,7 @@ public:
                 m[k] ++;
                 other_points = max(m[k], other_points);
             }
-            ans = max(ans, max(other_points, vertical_points) + start_points);
+            ans = max(ans, max(other_points, vertical_points));
         }
         return ans;
     }
