da98fe9b286e4601563d833cebd45837bb188195
zykzyk1993 <584853578@qq.com>
2021-10-29 13:06:51 +0800

    Update 160.cpp

--- code/160.cpp
+++ code/160.cpp
@@ -52,3 +52,15 @@ public:
         return NULL;
     }
 };
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *p1 = headA, *p2 = headB;
+        while(p1!=p2){
+            p1 = p1?p1->next:headB;
+            p2 = p2?p2->next:headA;
+        }
+        return p1;
+    }
+};
