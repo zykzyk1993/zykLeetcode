40da2f52774857e56e67b1c21202d1e24d073e35
zykzyk1993 <584853578@qq.com>
2021-11-02 22:54:59 +0800

    Update 094.cpp

--- code/094.cpp
+++ code/094.cpp
@@ -1,10 +1,27 @@
 /*
  * 94. 二叉树中序遍历
- * 非递归二叉树中序遍历
+ * 题意：非递归二叉树中序遍历
  * 难度：中等
  * 分类：树 栈
  * 算法：经典问题，注意压栈然后每次一直走到最左
  */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& res) {
+        if (!root) {
+            return;
+        }
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        inorder(root, res);
+        return res;
+    }
+};
+
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
