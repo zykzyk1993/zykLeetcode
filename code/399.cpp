619720dbfeba0055645d31fca7b1730dd83f7713
zykzyk1993 <584853578@qq.com>
2021-11-28 20:41:53 +0800

    Update 399.cpp

--- code/399.cpp
+++ code/399.cpp
@@ -5,6 +5,39 @@
  * 分类：图 并查集
  * 算法：建hash表保存等式以及邻接，然后在图中使用宽度优先遍历
  */
+//2021 floyd算法
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_map<string,int> hash;
+        int cnt=0;
+        int n=equations.size();
+        for(int i=0;i<n;i++){
+            if(hash.find(equations[i][0])==hash.end())hash[equations[i][0]]=cnt++;
+            if(hash.find(equations[i][1])==hash.end())hash[equations[i][1]]=cnt++;
+        }
+        vector<vector<double>> graph(cnt,vector<double>(cnt,-1));
+        for(int i=0;i<n;i++){
+            graph[hash[equations[i][0]]][hash[equations[i][1]]]=values[i];
+            graph[hash[equations[i][1]]][hash[equations[i][0]]]=1.0/values[i];
+            graph[i][i]=1;
+        }
+        for(int k=0;k<cnt;k++){
+            for(int i=0;i<cnt;i++){
+                for(int j=0;j<cnt;j++){
+                    if(graph[i][j]<0&&graph[i][k]>=0&&graph[k][j]>=0)graph[i][j]=graph[i][k]*graph[k][j];
+                }
+            }
+        }
+        vector<double> ans;
+        for(int i=0;i<queries.size();i++){
+            if(hash.find(queries[i][0])==hash.end()||hash.find(queries[i][1])==hash.end())ans.push_back(-1);
+            else ans.push_back(graph[hash[queries[i][0]]][hash[queries[i][1]]]);
+        }
+        return ans;
+    }
+};
+
 class Solution {
 public:
     double cal(string a, string b, map<pair<string,string>,double> &m, map<string,vector<string>> &eqs){
